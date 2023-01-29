1e64e24ea6740b9fa7382bedfa95d1298a7db286
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-01-29 16:28:54 +0300

    16:27 29.01.2023 Added react and configured scss loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,13 +2,23 @@ import webpack from 'webpack';
 
 export const buildLoaders = (): webpack.RuleSetRule[] => {
 
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+    };
+
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-    }
+    };
 
     return  [
         typescriptLoader,
+        cssLoader
     ]
 };
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,7 +2,7 @@ import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import { BuildOptions } from './types/config';
 
-export const buildPlugins = ({ paths}: BuildOptions): webpack.WebpackPluginInstance[] => {
+export const buildPlugins = ({ paths }: BuildOptions): webpack.WebpackPluginInstance[] => {
 
     return [
         new HTMLWebpackPlugin({
