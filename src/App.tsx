ffc63258ec692b5bb7c1fea685501cff54a2144e
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-01-29 18:37:29 +0300

    18:37 29.01.2023 Added css modules for files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.scss';
+import React from 'react';
+import { Counter } from './components/Counter';
+
+export const App = () => {
+    return (
+        <div className='app'>
+            123
+            <Counter/>
+        </div>
+    )
+}
