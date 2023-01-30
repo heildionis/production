be7fe347d4e878fd5c1270f2e69f8f5a89936ee3
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-01-30 04:21:47 +0300

    4:21 30.01.2023 Added theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import './index.scss';
-import React, { Suspense } from 'react';
+import './styles/index.scss';
+import React, { Suspense, useContext, useState } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Theme, ThemeContext } from './themes/ThemeContext';
+import { useTheme } from './themes/useTheme';
+
+
 
 export const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О нас</Link>
             <Suspense fallback={<div>Loading...</div>}>
