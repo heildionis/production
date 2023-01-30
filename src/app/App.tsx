b038802a437673369b09eba752f4e12476038593
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-01-30 15:30:10 +0300

    15:29 30.01.2023 App now based on Feature-Sliced Design architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,10 @@ import './styles/index.scss';
 import React, { Suspense } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { useTheme } from './themes/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 export const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -17,8 +17,8 @@ export const App = () => {
             <Link to={'/about'}>О нас</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path='/about' element={<AboutPageAsync/>}/>
-                    <Route path='/' element={<MainPageAsync/>}/>
+                    <Route path='/about' element={<AboutPage/>}/>
+                    <Route path='/' element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
