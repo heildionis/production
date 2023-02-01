b696010e150244280d9b5e88a997bad442d82b39
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-01 16:35:16 +0300

    16:34 01.02.2023 Add RegEx to find modules scss

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,23 +3,22 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
 
 export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => {
-
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
-                loader: "css-loader",
+                loader: 'css-loader',
                 options: {
                     modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev 
-                            ? '[path][name]__[local]--[hash:base64:5]' 
-                            : '[hash:base64:8]'
-                    }
-                }
+                        auto: /\.module\./,
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]',
+                    },
+                },
             },
-            "sass-loader",
+            'sass-loader',
         ],
     };
 
@@ -29,8 +28,5 @@ export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => 
         exclude: /node_modules/,
     };
 
-    return  [
-        typescriptLoader,
-        cssLoader
-    ]
+    return [typescriptLoader, cssLoader];
 };
