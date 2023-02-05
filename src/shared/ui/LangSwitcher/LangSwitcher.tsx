a8f5a79f799ffdc16bd78d332f1a05cc25862cd9
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-05 19:37:39 +0300

    19:37 05.02.2023 Created LangSwitcher component and added styles to Button component

---
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from '../Button/Button';
+import cls from './LangSwitcher.module.scss';
+
+interface LangSwitcherProps {
+    className?: string;
+}
+
+export const LangSwitcher: FC<LangSwitcherProps> = ({ className }) => {
+    const { t, i18n } = useTranslation();
+
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+    };
+
+    return (
+        <Button
+            theme='clear'
+            className={classNames(cls.LangSwitcher, {}, [className])}
+            onClick={toggle}
+        >
+            {t('Язык')}
+        </Button>
+    );
+};
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,4 @@
 export { AppLink } from './AppLink/AppLink';
 export { ThemeSwitcher } from './ThemeSwitcher/ThemeSwitcher';
 export { Button } from './Button/Button';
+export { LangSwitcher } from './LangSwitcher/LangSwitcher';
