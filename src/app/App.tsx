30f05296c63e4691d49c51fc19b220d03937b6e0
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-05 19:34:47 +0300

    19:34 05.02.2023 Defined IsDev flag in types and wrapped App with i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,24 @@
 import './styles/index.scss';
 
 import { useTheme } from 'app/providers/ThemeProvider';
-import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
+import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
 
 export const App = () => {
     const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className='content-page'>
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback=''>
+                <Navbar />
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
