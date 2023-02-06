bce9f86f7d670abcadd45f8726402d4f8bd2f54c
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-06 05:53:28 +0300

    5:53 06.02.2023 Refactored

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
+import cls from './LangSwitcher.module.scss';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from '../Button/Button';
-import cls from './LangSwitcher.module.scss';
 
 interface LangSwitcherProps {
     className?: string;
--- src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -6,7 +6,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 
-import { Button } from '..';
+import { Button } from '../Button/Button';
 
 interface ThemeSwitcherProps {
     className?: string;
