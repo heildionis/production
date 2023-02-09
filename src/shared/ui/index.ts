99de2a14b35ecc040ab2b9e6acac67953b3d59ca
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-09 11:49:52 +0300

    11:49 09.02.2023 Added Loader

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,3 +2,4 @@ export { AppLink } from './AppLink/AppLink';
 export { ThemeSwitcher } from './ThemeSwitcher/ThemeSwitcher';
 export { Button } from './Button/Button';
 export { LangSwitcher } from './LangSwitcher/LangSwitcher';
+export { Loader } from './Loader/Loader';
