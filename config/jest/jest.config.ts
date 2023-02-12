fb71632ebbedef84fa647e20a2f0e1f34fe6a38b
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-12 17:37:56 +0300

    17:37 12.02.2023 Configured jest config and added react testing librari with jest dom

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,21 +1,11 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
+import path from 'path';
 
 export default {
-    // All imported modules in your tests should be mocked automatically
-    // automock: false,
-
-    // Stop running tests after `n` failures
-    // bail: 0,
-
-    // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "C:\\Users\\densa\\AppData\\Local\\Temp\\jest",
-
-    // Automatically clear mock calls, instances, contexts and results before every test
     clearMocks: true,
     testEnvironment: 'jsdom',
+    coveragePathIgnorePatterns: [
+        '\\\\node_modules\\\\'
+    ],
     moduleFileExtensions: [
         'js',
         'mjs',
@@ -27,15 +17,21 @@ export default {
         'node'
     ],
     moduleDirectories: [
-        'node_modules'
+        'node_modules',
+        // 'src'
     ],
-    coveragePathIgnorePatterns: [
-        '\\\\node_modules\\\\'
+    modulePaths: [
+        '<rootDir>src',
     ],
-    rootDir: '../../',
     testMatch: [
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'
     ],
+    rootDir: '../../',
+    setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+    moduleNameMapper: {
+        '\\.(s?css|less)$': 'identity-obj-proxy',
+        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx')
+    },
 
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
