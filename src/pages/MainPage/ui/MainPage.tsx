e3cfd6b0bb3c9979c56407df1c2af2d2ca92a952
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-23 14:08:33 +0300

    14:08 23.02.2023 Refactored

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,8 +1,8 @@
-import { Counter } from 'entities/Counter';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
     const { t } = useTranslation();
+
     return (
         <div>
             {t('Главная страница')}
