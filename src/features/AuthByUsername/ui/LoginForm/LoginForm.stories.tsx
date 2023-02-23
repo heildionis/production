10163128a0ad28c39227b90abe7dabcbfabf352c
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-23 23:12:45 +0300

    23:13 23.02.2023 Created login form with state

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator';
 import { LoginForm } from './LoginForm';
 
 export default {
@@ -15,11 +16,66 @@ export default {
 const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {
-};
-Primary.decorators = [ThemeDecorator(Theme.LIGHT)];
+Primary.decorators = [StoreDecorator({
+    loginForm: {
+        username: 'name',
+        password: 'password',
+    },
+})];
 
 export const Secondary = Template.bind({});
-Secondary.args = {
-};
-Secondary.decorators = [ThemeDecorator(Theme.DARK)];
+Secondary.decorators = [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+        loginForm: {
+            username: 'name',
+            password: 'password',
+        },
+    }),
+];
+
+export const withError = Template.bind({});
+withError.decorators = [
+    StoreDecorator({
+        loginForm: {
+            username: 'name',
+            password: 'password',
+            error: 'message',
+        },
+    }),
+];
+
+export const withErrorDark = Template.bind({});
+withErrorDark.decorators = [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+        loginForm: {
+            username: 'name',
+            password: 'password',
+            error: 'message',
+        },
+    }),
+];
+
+export const Loading = Template.bind({});
+Loading.decorators = [
+    StoreDecorator({
+        loginForm: {
+            username: 'name',
+            password: 'password',
+            isLoading: true,
+        },
+    }),
+];
+
+export const LoadingDark = Template.bind({});
+LoadingDark.decorators = [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+        loginForm: {
+            username: 'name',
+            password: 'password',
+            isLoading: true,
+        },
+    }),
+];
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,32 +1,66 @@
-import { FC } from 'react';
+import { FC, memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { Button, Input } from 'shared/ui';
+import { Button, Input, Text } from 'shared/ui';
+import { ButtonTheme } from 'shared/ui/Button/Button';
+import { TextTheme } from 'shared/ui/Text/Text';
+import { loginByUsername } from '../../model/service/loginByUsername/loginByUsername';
+import { loginActions } from '../../model/slice/loginSlice';
+import { getLoginState } from '../../model/selectors/getLoginState/getLoginState';
+
 import cls from './LoginForm.module.scss';
 
 interface LoginFormProps {
     className?: string;
 }
 
-export const LoginForm: FC<LoginFormProps> = ({ className }) => {
+export const LoginForm: FC<LoginFormProps> = memo(({ className }: LoginFormProps) => {
     const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const {
+        username, password, error, isLoading,
+    } = useSelector(getLoginState);
+
+    const onChangeUsername = useCallback((value: string) => {
+        dispatch(loginActions.setUsername(value));
+    }, [dispatch]);
+
+    const onChangePassword = useCallback((value: string) => {
+        dispatch(loginActions.setPassword(value));
+    }, [dispatch]);
+
+    const onLoginClick = useCallback(() => {
+        dispatch(loginByUsername({ username, password }));
+    }, [dispatch, password, username]);
 
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
+            <Text title={t('Форма авторизации')} />
+            {error && <Text text={t('Некорректное имя пользователя или пароль')} theme={TextTheme.ERROR} />}
             <Input
                 autoFocus
                 type='text'
                 placeholder={t('Введите username')}
                 className={cls.input}
+                onChange={onChangeUsername}
+                value={username}
             />
             <Input
                 type='text'
                 placeholder={t('Введите пароль')}
                 className={cls.input}
+                onChange={onChangePassword}
+                value={password}
             />
-            <Button className={cls.loginBtn}>
+            <Button
+                onClick={onLoginClick}
+                theme={ButtonTheme.OUTLINE}
+                className={cls.loginBtn}
+                disabled={isLoading}
+            >
                 {t('Войти')}
             </Button>
         </div>
     );
-};
+});
