6defdde8a620c6a95f1123daba1115408042b8ae
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-26 21:23:29 +0300

    21:23 26.02.2023 Fixed confid, created custom module loader

--- src/shared/config/i18n/i18nForTests.ts
+++ src/shared/config/i18n/i18nForTests.ts
@@ -8,7 +8,7 @@ i18n
         fallbackLng: 'ru',
         debug: false,
         interpolation: {
-            escapeValue: false, // not needed for react!!
+            escapeValue: false,
         },
 
         resources: { ru: { translations: {} } },
