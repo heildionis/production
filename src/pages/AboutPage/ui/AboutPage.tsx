cca6d1c419a0d6a307a9ae28eae0983ba8667e3a
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-02-28 23:27:02 +0300

    23:25 28.02.2023 Created profie page/memoized all pages

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,8 +1,9 @@
+import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
-export const AboutPage = () => {
+export const AboutPage = memo(() => {
     const { t } = useTranslation('about');
     return <div>{t('О сайте')}</div>;
-};
+});
 
 export default AboutPage;
---
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator';
+import ProfilePage from './ProfilePage';
+
+export default {
+    title: 'pages/ProfilePage',
+    component: ProfilePage,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    decorators: [StoreDecorator({})],
+} as ComponentMeta<typeof ProfilePage>;
+
+const Template: ComponentStory<typeof ProfilePage> = (args) => <ProfilePage />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
+
+export const Light = Template.bind({});
+Light.args = {};
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
