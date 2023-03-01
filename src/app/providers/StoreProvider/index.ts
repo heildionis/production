c0fe15e73b77827289cbeae2ba0b9c2f7eec0b15
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-01 23:06:32 +0300

    23:06 01.03.2023 Added extraArg for thunk

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,3 +1,8 @@
-export { StateSchema, ReduxStoreWithManager } from './config/StateSchema';
+export {
+    StateSchema,
+    ReduxStoreWithManager,
+    ThunkExtraArg,
+    ThunkConfig,
+} from './config/StateSchema';
 export { createReduxStore, AppDispatch } from './config/store';
 export { StoreProvider } from './ui/StoreProvider';
--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,6 +1,7 @@
 import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { FC, ReactNode } from 'react';
 import { Provider } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { StateSchema } from '../config/StateSchema';
 import { createReduxStore } from '../config/store';
 
@@ -17,9 +18,12 @@ export const StoreProvider: FC<StoreProviderProps> = (props) => {
         asyncReducers,
     } = props;
 
+    const navigate = useNavigate();
+
     const store = createReduxStore(
         initialState as StateSchema,
         asyncReducers as ReducersMapObject<StateSchema>,
+        navigate,
     );
 
     return (
