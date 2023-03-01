ffdd8ac4a9c10dc9d1dca6e668f5a36f7a91018c
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-01 23:01:31 +0300

    23:00 01.03.2023 Refactored with thunk extra

--- src/features/AuthByUsername/model/service/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/service/loginByUsername/loginByUsername.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { ThunkConfig } from 'app/providers/StoreProvider';
 import { User, userActions } from 'entities/User';
 import { USER_LOCALSTORAGE_KEY } from 'shared/constants/localstorage';
 
@@ -8,26 +8,26 @@ interface LoginByUsernameProps {
     password: string;
 }
 
-export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { rejectValue: string }>(
+export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, ThunkConfig<string>>(
     'login/loginByUsername',
-    async ({ username, password }, thunkAPI) => {
+    async (authData, thunkAPI) => {
+        const { dispatch, extra, rejectWithValue } = thunkAPI;
+
         try {
-            const response = await axios.post<User>('http://localhost:5000/login', {
-                username,
-                password,
-            });
+            const response = await extra.api.post<User>('http://localhost:5000/login', authData);
 
             if (!response.data) {
                 throw new Error();
             }
 
             localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-            thunkAPI.dispatch(userActions.setAuthData(response.data));
+            dispatch(userActions.setAuthData(response.data));
+            extra.navigate('/about');
 
             return response.data;
         } catch (error) {
             console.log(error);
-            return thunkAPI.rejectWithValue('error');
+            return rejectWithValue('error');
         }
     },
 );
