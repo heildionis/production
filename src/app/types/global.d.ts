75bdb9f52c5f490a97d30e1965b6758f664b9327
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-02 18:48:27 +0300

    18:48 02.03.2023 Fixed strict mode errors

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -17,3 +17,7 @@ declare module '*.svg' {
 
 declare const __IS_DEV__: boolean;
 declare const __API__: string;
+
+type DeepPartial<T> = {
+    [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K]
+}
