ae471f05cfa01594444671dc3025296f3ba07564
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-02 18:49:36 +0300

    18:49 02.03.2023 Added profile thunk for fetching data

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -7,3 +7,11 @@ export {
     Profile,
     ProfileSchema,
 } from './model/types/profile';
+
+export {
+    fetchProfileData,
+} from './model/services/fetchProfileData/fetchProfileData';
+
+export {
+    ProfileCard,
+} from './ui/ProfileCard/ProfileCard';
---
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Button, Input, Text } from 'shared/ui';
+import { ButtonTheme } from 'shared/ui/Button/Button';
+import { getProfileIsLoading } from '../../model/selectors/getProfileIsLoading/getProfileIsLoading';
+import { getProfileError } from '../../model/selectors/getProfileError/getProfileError';
+import { getProfileData } from '../../model/selectors/getProfileData/getProfileData';
+import cls from './ProfileCard.module.scss';
+
+interface ProfileCardProps {
+    className?: string;
+}
+
+export const ProfileCard: FC<ProfileCardProps> = ({ className }) => {
+    const { t } = useTranslation('profile');
+    const data = useSelector(getProfileData);
+    const isLoading = useSelector(getProfileIsLoading);
+    const error = useSelector(getProfileError);
+
+    return (
+        <div className={classNames(cls.ProfileCard, {}, [className])}>
+            <div className={cls.header}>
+                <Text title={t('Профиль')} />
+                <Button
+                    className={cls.editBtn}
+                    theme={ButtonTheme.OUTLINE}
+                >
+                    {t('Редактировать')}
+                </Button>
+            </div>
+            <div className={cls.data}>
+                <Input
+                    className={cls.input}
+                    value={data?.first}
+                    placeholder={t('Ваше имя')}
+                />
+                <Input
+                    className={cls.input}
+                    value={data?.lastname}
+                    placeholder={t('Ваша фамилия')}
+                />
+            </div>
+        </div>
+    );
+};
