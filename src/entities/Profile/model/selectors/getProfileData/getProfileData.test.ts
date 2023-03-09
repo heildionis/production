c492b933fe9f0ae00acda3f92d308a00b0962f53
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-09 14:23:17 +0300

    14:23 09.03.2023 Testing for selectors and slice

---
+++ src/entities/Profile/model/selectors/getProfileData/getProfileData.test.ts
@@ -0,0 +1,20 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+import { mockedProfileJest } from '../../../lib/tests/mockedProfileJest';
+import { getProfileData } from './getProfileData';
+
+describe('getProfileData.test', () => {
+    test('should work with filled state', () => {
+        const data = { ...mockedProfileJest };
+        const state: DeepPartial<StateSchema> = {
+            profile: {
+                data,
+            },
+        };
+        expect(getProfileData(state as StateSchema)).toEqual(data);
+    });
+
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getProfileData(state as StateSchema)).toEqual(undefined);
+    });
+});
--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -19,6 +19,7 @@ export const profileSlice = createSlice({
         },
         cancelEdit: (state) => {
             state.readonly = true;
+            state.validateErrors = undefined;
             state.form = state.data;
         },
         updateProfile: (state, action: PayloadAction<Profile>) => {
@@ -48,6 +49,7 @@ export const profileSlice = createSlice({
             })
             .addCase(updateProfileData.pending, (state) => {
                 state.error = undefined;
+                state.validateErrors = undefined;
                 state.isLoading = true;
             })
             .addCase(updateProfileData.fulfilled, (
@@ -57,11 +59,12 @@ export const profileSlice = createSlice({
                 state.isLoading = false;
                 state.data = action.payload;
                 state.form = action.payload;
+                state.validateErrors = undefined;
                 state.readonly = true;
             })
             .addCase(updateProfileData.rejected, (state, action) => {
                 state.isLoading = false;
-                state.error = action.payload;
+                state.validateErrors = action.payload;
             });
     },
 });
