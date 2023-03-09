d740a4f3e113818b739e48b8a036b1c9e62d6c55
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-09 14:22:16 +0300

    14:22 09.03.2023 Added tests for thunks

--- src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
+++ src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
@@ -9,6 +9,7 @@ export const fetchProfileData = createAsyncThunk<Profile, void, ThunkConfig<stri
 
         try {
             const response = await extra.api.get<Profile>('/profile');
+
             if (!response.data) {
                 throw new Error();
             }
