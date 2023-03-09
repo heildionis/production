090396b4fe9e2e483a922be353e22fba1e1031bf
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-09 14:20:41 +0300

    14:20 09.03.2023 Added mock data for testing

---
+++ src/entities/Profile/lib/tests/mockedProfile.ts
@@ -0,0 +1,14 @@
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+import avatar from 'shared/assets/tests/storybook.jpg';
+
+export const mockedProfileData = {
+    first: 'Denis',
+    lastname: 'Sarzhan',
+    age: 18,
+    city: 'Donetsk',
+    username: 'neospectrum',
+    country: Country.Ukraine,
+    currency: Currency.RUB,
+    avatar,
+};
---
+++ src/entities/Profile/lib/tests/mockedProfileJest.ts
@@ -0,0 +1,12 @@
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+
+export const mockedProfileJest = {
+    first: 'Denis',
+    lastname: 'Sarzhan',
+    age: 18,
+    city: 'Donetsk',
+    username: 'neospectrum',
+    country: Country.Ukraine,
+    currency: Currency.RUB,
+};
