1c29a960ce64fb4bebec927c464b389642ec890b
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-13 16:12:20 +0300

    16:11 13.03.2023 Created require auth component, redirect while user is auth on main page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,11 +4,12 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/User';
+import { useDispatch, useSelector } from 'react-redux';
+import { userActions, getUserInited } from 'entities/User';
 
 export const App = () => {
     const dispatch = useDispatch();
+    const inited = useSelector(getUserInited);
 
     useEffect(() => {
         dispatch(userActions.initAuthData());
@@ -20,7 +21,7 @@ export const App = () => {
                 <Navbar />
                 <div className='content-page'>
                     <Sidebar />
-                    <AppRouter />
+                    {inited && <AppRouter />}
                 </div>
             </Suspense>
         </div>
---
+++ src/app/providers/router/ui/RequireAuth.tsx
@@ -0,0 +1,26 @@
+import { getUserAuthData } from 'entities/User';
+import { FC, ReactNode } from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, useLocation } from 'react-router-dom';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+
+interface RequireAuthProps {
+    children: JSX.Element;
+}
+
+export const RequireAuth: FC<RequireAuthProps> = ({ children }) => {
+    const location = useLocation();
+    const auth = useSelector(getUserAuthData);
+
+    if (!auth) {
+        return (
+            <Navigate
+                to={RoutePath.main}
+                state={{ from: location }}
+                replace
+            />
+        );
+    }
+
+    return (children);
+};
