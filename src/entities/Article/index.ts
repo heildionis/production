c7532c6b01e4dd6a795829047a04cbba96372558
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-14 04:36:42 +0300

    04:36 14.03.2023 Created articleDetails reducer

---
+++ src/entities/Article/index.ts
@@ -0,0 +1,5 @@
+export { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
+
+export { Article } from './model/types/article';
+
+export { ArticleDetails } from './ui/ArticleDetails/ArticleDetails';
