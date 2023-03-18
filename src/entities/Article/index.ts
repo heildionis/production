ec17ed2c20e874858249e44c4a1ad1da2a032d9b
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-18 19:36:22 +0300

    19:36 18.03.2023 Public api

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,3 +1,5 @@
+export { articleDetailsReducer } from './model/slice/articleDetailsSlice';
+
 export { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
 
 export { Article } from './model/types/article';
