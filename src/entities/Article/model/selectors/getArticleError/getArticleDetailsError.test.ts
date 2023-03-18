df35c19173e16c28b378e9fe455bd92784806e17
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-18 19:33:45 +0300

    19:33 18.03.2023 Added selector tests

---
+++ src/entities/Article/model/selectors/getArticleError/getArticleDetailsError.test.ts
@@ -0,0 +1,18 @@
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { getArticleDetailsError } from './getArticleDetailsError';
+
+describe('getArticleDetailsError.test', () => {
+    test('To have error', () => {
+        const state: DeepPartial<StateSchema> = {
+            articleDetails: {
+                error: 'error',
+            },
+        };
+        expect(getArticleDetailsError(state as StateSchema)).toBe('error');
+    });
+
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getArticleDetailsError(state as StateSchema)).toBe(undefined);
+    });
+});
---
+++ src/entities/Article/model/selectors/getArticleIsLoading/getArticleDetailsIsLoading.test.ts
@@ -0,0 +1,18 @@
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { getArticleDetailsIsLoading } from './getArticleDetailsIsLoading';
+
+describe('getArticleDetailsIsLoading.test', () => {
+    test('should return true while loading', () => {
+        const state: DeepPartial<StateSchema> = {
+            articleDetails: {
+                isLoading: true,
+            },
+        };
+        expect(getArticleDetailsIsLoading(state as StateSchema)).toBe(true);
+    });
+
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getArticleDetailsIsLoading(state as StateSchema)).toBe(undefined);
+    });
+});
