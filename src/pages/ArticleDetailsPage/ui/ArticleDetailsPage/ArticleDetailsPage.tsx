ffa6fd54cd0dc42cd841ea229df80778d890192d
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-19 20:45:57 +0300

    20:45 19.03.2023 Added comments to page

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,18 +1,53 @@
-import { ArticleDetails } from 'entities/Article';
+import { ArticleDetails, getArticleDetailsData } from 'entities/Article';
+import { CommentList } from 'entities/Comment';
 import { FC, memo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
+import {
+    DynamicModuleLoader,
+    ReducersList,
+} from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { Text } from 'shared/ui';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import {
+    fetchCommentsByArticleId,
+} from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+import {
+    getArticleCommentsIsLoading,
+} from '../../model/selectors/comments/getArticleCommentsIsLoading/getArticleCommentsIsLoading';
+import {
+    getArticleCommentsError,
+} from '../../model/selectors/comments/getArticleCommentsError/getArticleCommentsError';
+import {
+    articleDetailsCommentReducer,
+    getArticleComments,
+} from '../../model/slices/articleDetailsCommentSlice';
 import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
    className?: string;
 }
 
+const reducers: ReducersList = {
+    articleDeatilsComments: articleDetailsCommentReducer,
+};
+
 const ArticleDetailsPage: FC<ArticleDetailsPageProps> = memo((props: ArticleDetailsPageProps) => {
     const { className } = props;
     const { t } = useTranslation('article-details');
     const { id } = useParams<{id: string}>();
+    const dispatch = useAppDispatch();
+    const article = useSelector(getArticleDetailsData);
+    const comments = useSelector(getArticleComments.selectAll);
+    const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
+    const commentsError = useSelector(getArticleCommentsError);
+
+    useInitialEffect(() => {
+        dispatch(fetchCommentsByArticleId(id));
+    });
 
     if (!id) {
         return (
@@ -23,9 +58,20 @@ const ArticleDetailsPage: FC<ArticleDetailsPageProps> = memo((props: ArticleDeta
     }
 
     return (
-        <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-            <ArticleDetails id={id} />
-        </div>
+        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+            <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+                <ArticleDetails id={id} />
+                {article && (
+                    <>
+                        <Text title={t('Комментарии')} />
+                        <CommentList
+                            isLoading={commentsIsLoading}
+                            comments={comments}
+                        />
+                    </>
+                )}
+            </div>
+        </DynamicModuleLoader>
     );
 });
 
