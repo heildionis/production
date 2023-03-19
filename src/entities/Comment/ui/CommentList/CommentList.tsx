9d16fbf935a0b34a714c0dd60fc448fd3316773f
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-19 20:40:37 +0300

    20:40 19.03.2023 Created comment entity

---
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Text } from 'shared/ui';
+import cls from './CommentList.module.scss';
+import { Comment } from '../../model/types/comment';
+import { CommentCard } from '../CommentCard/CommentCard';
+
+interface CommentListProps {
+   className?: string;
+   comments: Comment[];
+   isLoading?: boolean
+}
+
+export const CommentList: FC<CommentListProps> = (props) => {
+    const { className, comments, isLoading } = props;
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.CommentList, {}, [className])}>
+            {comments?.length
+                ? (comments.map((comment) => (
+                    <CommentCard
+                        isLoading={isLoading}
+                        className={cls.comment}
+                        key={comment.id}
+                        comment={comment}
+                    />
+                )))
+                : <Text text={t('Комментарии отсутсвуют')} />}
+        </div>
+    );
+};
