cf9dc984b7efb7418d0636804e927288fd56742b
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-19 20:45:08 +0300

    20:44 19.03.2023 Created articleDetailsCommentReducer with fetching comments by article id

---
+++ src/pages/ArticleDetailsPage/model/slices/articleDetailsCommentSlice.ts
@@ -0,0 +1,51 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+import {
+    createEntityAdapter,
+    createSlice,
+    PayloadAction,
+} from '@reduxjs/toolkit';
+import { Comment } from 'entities/Comment';
+import { ArticleDetailsCommentSchema } from '../types/AritcleDeatilsCommentSchema';
+import { fetchCommentsByArticleId } from '../services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+
+const commentsAdapter = createEntityAdapter<Comment>({
+    selectId: (comment) => comment.id,
+});
+
+export const getArticleComments = commentsAdapter.getSelectors<StateSchema>(
+    (state) => state.articleDeatilsComments || commentsAdapter.getInitialState(),
+);
+
+const articleDetailsCommentSlice = createSlice({
+    name: 'articleDetailsComments',
+    initialState: commentsAdapter.getInitialState<ArticleDetailsCommentSchema>({
+        isLoading: false,
+        error: undefined,
+        ids: [],
+        entities: {},
+    }),
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchCommentsByArticleId.pending, (state) => {
+                state.error = undefined;
+                state.isLoading = true;
+            })
+            .addCase(fetchCommentsByArticleId.fulfilled, (
+                state,
+                action: PayloadAction<Comment[]>,
+            ) => {
+                state.isLoading = false;
+                commentsAdapter.setAll(state, action.payload);
+            })
+            .addCase(fetchCommentsByArticleId.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
+    },
+});
+
+export const { actions: articleDetailsCommentActions } = articleDetailsCommentSlice;
+export const { reducer: articleDetailsCommentReducer } = articleDetailsCommentSlice;
