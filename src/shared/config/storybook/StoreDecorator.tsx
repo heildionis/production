96cbbd40a3289a732c2555356e00e47c37552a88
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-22 23:52:23 +0300

    23:52 22.03.2023 Updated config

--- src/shared/config/storybook/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator.tsx
@@ -3,6 +3,7 @@ import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import { articleDetailsReducer } from 'entities/Article';
 import { profileReducer } from 'entities/Profile';
+import { addCommentFormReducer } from 'features/addCommentForm/model/slice/addCommentFormSlice';
 import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 import { ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 
@@ -10,6 +11,8 @@ const defaultAsyncReducers: ReducersList = {
     loginForm: loginReducer,
     profile: profileReducer,
     articleDetails: articleDetailsReducer,
+    addCommentForm: addCommentFormReducer,
+    articleDeatilsComments: articleDetailsReducer,
 };
 
 export const StoreDecorator = (
