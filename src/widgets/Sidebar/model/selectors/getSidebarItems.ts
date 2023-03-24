fd1b35a366efc95b387dd17f26971e75dd7277e4
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-24 17:58:21 +0300

    17:58 24.03.2023 Refactored model with selector

---
+++ src/widgets/Sidebar/model/selectors/getSidebarItems.ts
@@ -0,0 +1,46 @@
+import { getUserAuthData } from 'entities/User';
+import { createSelector } from '@reduxjs/toolkit';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import AboutIcon from 'shared/assets/icons/list.svg';
+import MainIcon from 'shared/assets/icons/home.svg';
+import ProfileIcon from 'shared/assets/icons/profile.svg';
+import ArticlesIcon from 'shared/assets/icons/article.svg';
+import { SidebarItemType } from '../types/sidebar';
+
+export const getSidebarItems = createSelector(
+    getUserAuthData,
+    (userData) => {
+        const sidebarItemsList: SidebarItemType[] = [
+            {
+                path: RoutePath.main,
+                Icon: MainIcon,
+                text: 'Главная',
+            },
+            {
+                path: RoutePath.about,
+                Icon: AboutIcon,
+                text: 'О сайте',
+            },
+
+        ];
+
+        if (userData) {
+            sidebarItemsList.push(
+                {
+                    path: RoutePath.profile + userData.id,
+                    Icon: ProfileIcon,
+                    text: 'Профиль',
+                    authOnly: true,
+                },
+                {
+                    path: RoutePath.articles,
+                    Icon: ArticlesIcon,
+                    text: 'Статьи',
+                    authOnly: true,
+                },
+            );
+        }
+
+        return sidebarItemsList;
+    },
+);
