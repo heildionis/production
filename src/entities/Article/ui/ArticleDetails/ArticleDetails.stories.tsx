15e87777a05e3941c58272f525d63f877a457d4f
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-24 23:00:36 +0300

    23:00 24.03.2023 Refactored stories

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.stories.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.stories.tsx
@@ -11,6 +11,10 @@ const article: Article = {
     views: 1022,
     createdAt: '26.02.2022',
     type: [ArticleType.IT],
+    user: {
+        id: '1',
+        username: 'Denis Sarzhan',
+    },
     blocks: [
         {
             id: 1,
@@ -74,7 +78,7 @@ const article: Article = {
 };
 
 export default {
-    title: 'entities/Article',
+    title: 'entities/Article/ArticleDetails',
     component: ArticleDetails,
     argTypes: {
         backgroundColor: { control: 'color' },
