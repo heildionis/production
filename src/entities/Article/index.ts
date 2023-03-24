9f2019fb4094a44f838b65c0ceea31c8ff7c5697
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-03-24 23:00:54 +0300

    23:00 24.03.2023 Extend public api

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,5 +1,6 @@
+export { ArticleList } from './ui/ArticleList/ArticleList';
 export { getArticleDetailsData } from './model/selectors/getArticleDetailsData/getArticleDetailsData';
 export { articleDetailsReducer } from './model/slice/articleDetailsSlice';
 export { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
-export { Article } from './model/types/article';
+export { Article, ArticleView } from './model/types/article';
 export { ArticleDetails } from './ui/ArticleDetails/ArticleDetails';
