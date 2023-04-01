cdd614d4688f8577295a2e3f15418994948a3fdf
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-01 03:51:05 +0300

    03:51 01.04.2023 Created filters for article

---
+++ src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
@@ -0,0 +1,79 @@
+import { FC, memo, useCallback, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Select } from 'shared/ui';
+import { SelectOption } from 'shared/ui/Select/Select';
+import { SortOrder } from 'shared/types/sort';
+import cls from './ArticleSortSelector.module.scss';
+import { ArticleSortField } from '../../model/types/article';
+
+interface ArticleSortSelectorProps {
+    className?: string;
+    sort: ArticleSortField;
+    order: SortOrder;
+    onChangeOrder: (newOrder: SortOrder) => void;
+    onChangeSort: (newSort: ArticleSortField) => void;
+}
+
+export const ArticleSortSelector: FC<ArticleSortSelectorProps> = memo((props: ArticleSortSelectorProps) => {
+    const {
+        className,
+        sort,
+        order,
+        onChangeOrder,
+        onChangeSort,
+    } = props;
+    const { t } = useTranslation();
+
+    const orderOptions = useMemo<SelectOption[]>(() => ([
+        {
+            value: 'asc',
+            content: t('возрастанию'),
+        },
+        {
+            value: 'desc',
+            content: t('убыванию'),
+        },
+    ]), [t]);
+
+    const sortFieldOptions = useMemo<SelectOption[]>(() => [
+        {
+            value: ArticleSortField.CREATED,
+            content: t('дата создания'),
+        },
+        {
+            value: ArticleSortField.TITLE,
+            content: t('названию'),
+        },
+        {
+            value: ArticleSortField.VIEWS,
+            content: t('просмотрам'),
+        },
+    ], [t]);
+
+    const changeSortHandler = useCallback((newSort: string) => {
+        onChangeSort(newSort as ArticleSortField);
+    }, [onChangeSort]);
+
+    const changeOrderHandler = useCallback((newOrder: string) => {
+        onChangeOrder(newOrder as SortOrder);
+    }, [onChangeOrder]);
+
+    return (
+        <div className={classNames(cls.ArticleSortSelector, {}, [className])}>
+            <Select
+                options={sortFieldOptions}
+                label={t('Сортировать по')}
+                value={sort}
+                onChange={changeSortHandler}
+            />
+            <Select
+                options={orderOptions}
+                label={t('По')}
+                value={order}
+                onChange={changeOrderHandler}
+                className={cls.order}
+            />
+        </div>
+    );
+});
