3aaded5890d20573b47734d1bd2ff7b4179def2e
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-01 03:53:14 +0300

    03:53 01.04.2023 Updated public api

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,7 +1,10 @@
+export { ArticleSortSelector } from './ui/ArticleSortSelector/ArticleSortSelector';
+export { ArticleSortField } from './model/types/article';
 export { ArticleViewSelector } from './ui/ArticleViewSelector/ArticleViewSelector';
 export { ArticleList } from './ui/ArticleList/ArticleList';
 export { getArticleDetailsData } from './model/selectors/getArticleDetailsData/getArticleDetailsData';
 export { articleDetailsReducer } from './model/slice/articleDetailsSlice';
 export { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
-export { Article, ArticleView } from './model/types/article';
+export { Article, ArticleView, ArticleType } from './model/types/article';
 export { ArticleDetails } from './ui/ArticleDetails/ArticleDetails';
+export { ArticleTypeTabs } from './ui/ArticleTypeTabs/ArticleTypeTabs';
