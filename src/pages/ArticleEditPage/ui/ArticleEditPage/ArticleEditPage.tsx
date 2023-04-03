b85d10a33252d1739ed6fe329d5becef4334f32e
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-03 22:36:42 +0300

    22:36 03.04.2023 Created article edit page

---
+++ src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.tsx
@@ -0,0 +1,25 @@
+import { FC, memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Page } from 'widgets/Page/Page';
+import { useParams } from 'react-router-dom';
+import cls from './ArticleEditPage.module.scss';
+
+interface ArticleEditPageProps {
+    className?: string;
+}
+
+const ArticleEditPage: FC<ArticleEditPageProps> = memo((props: ArticleEditPageProps) => {
+    const { className } = props;
+    const { t } = useTranslation();
+    const { id } = useParams<{id: string}>();
+    const isEdit = Boolean(id);
+
+    return (
+        <Page className={classNames(cls.ArticleEditPage, {}, [className])}>
+            {isEdit ? t('Редактировать') + id : t('Создать')}
+        </Page>
+    );
+});
+
+export default ArticleEditPage;
