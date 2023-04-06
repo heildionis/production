64b59553b46969d99b8fbba6988c01ea60a7b0a9
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-06 12:21:42 +0300

    12:21 06.04.2023 Added copy plugin and locales path to build

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import CopyPlugin from 'copy-webpack-plugin';
 import { type BuildOptions } from './types/config';
 
 export const buildPlugins = ({
@@ -21,6 +22,11 @@ export const buildPlugins = ({
             __IS_DEV__: JSON.stringify(isDev),
             __API__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project),
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: paths.locales, to: paths.buildLocales },
+            ],
         }),
     ];
 
