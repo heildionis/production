1f90b4987a31714514b8ed3ef668a81fe347e654
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-09 22:57:03 +0300

    22:56 09.04.2023 Added locales settings

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -11,6 +11,8 @@ export default ({ config }: { config: webpack.Configuration }): webpack.Configur
         build: '',
         html: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
+        locales: '',
+        buildLocales: '',
     };
 
     config.resolve?.modules?.push(paths.src);
