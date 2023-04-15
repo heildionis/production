c0a19ba1a93f67c2a7b1ab8ef59ee9a4e7fba3b9
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-16 01:16:32 +0300

    01:16 16.04.2023 Added require auth with roles

--- src/app/providers/router/ui/RequireAuth.tsx
+++ src/app/providers/router/ui/RequireAuth.tsx
@@ -1,21 +1,44 @@
-import { getUserAuthData } from 'entities/User';
-import { FC } from 'react';
+import { UserRole, getUserAuthData, getUserRoles } from 'entities/User';
+import { FC, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 
 interface RequireAuthProps {
     children: JSX.Element;
+    roles?: UserRole[];
 }
 
-export const RequireAuth: FC<RequireAuthProps> = ({ children }) => {
+export const RequireAuth: FC<RequireAuthProps> = ({ children, roles }) => {
     const location = useLocation();
     const auth = useSelector(getUserAuthData);
+    const userRoles = useSelector(getUserRoles);
+
+    const hasRequiredRoles = useMemo(() => {
+        if (!roles) {
+            return true;
+        }
+
+        return roles?.some((requiredRole) => {
+            const hasRole = userRoles?.includes(requiredRole);
+            return hasRole;
+        });
+    }, [roles, userRoles]);
 
     if (!auth) {
         return (
             <Navigate
                 to={RoutePath.main}
+                state={{ from: location }}
+                replace
+            />
+        );
+    }
+
+    if (!hasRequiredRoles) {
+        return (
+            <Navigate
+                to={RoutePath.forbidden}
                 state={{ from: location }}
                 replace
             />
