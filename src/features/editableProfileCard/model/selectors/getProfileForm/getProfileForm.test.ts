67b93e9ba2b9262dd22c5686ed4269f1a48ee828
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-15 21:35:15 +0300

    21:35 15.04.2023 Create features while refactor

---
+++ src/features/editableProfileCard/model/selectors/getProfileForm/getProfileForm.test.ts
@@ -0,0 +1,20 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+import { mockedProfileJest } from 'entities/Profile';
+import { getProfileForm } from './getProfileForm';
+
+describe('getProfileForm.test', () => {
+    test('should work with filled state', () => {
+        const form = { ...mockedProfileJest };
+        const state: DeepPartial<StateSchema> = {
+            profile: {
+                form,
+            },
+        };
+        expect(getProfileForm(state as StateSchema)).toEqual(form);
+    });
+
+    test('should work with empty state', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getProfileForm(state as StateSchema)).toEqual(undefined);
+    });
+});
---
+++ src/features/editableProfileCard/model/services/fetchProfileData/fetchProfileData.test.ts
@@ -0,0 +1,24 @@
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
+import { mockedProfileJest } from 'entities/Profile';
+import { fetchProfileData } from './fetchProfileData';
+
+describe('fetchProfileData.test', () => {
+    test('success', async () => {
+        const thunk = new TestAsyncThunk(fetchProfileData);
+
+        thunk.api.get.mockReturnValue(Promise.resolve({ data: { ...mockedProfileJest } }));
+        const result = await thunk.callThunk('1');
+
+        expect(thunk.api.get).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('fulfilled');
+        expect(result.payload).toEqual(mockedProfileJest);
+    });
+
+    test('error', async () => {
+        const thunk = new TestAsyncThunk(fetchProfileData);
+        thunk.api.get.mockReturnValue(Promise.resolve({ status: 403 }));
+        const result = await thunk.callThunk('1');
+
+        expect(result.meta.requestStatus).toBe('rejected');
+    });
+});
