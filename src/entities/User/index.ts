6176f380ab47323673366bc36cc350d4c5af0a10
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-16 01:17:19 +0300

    01:17 16.04.2023 Created selectors for user roles

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,5 +1,9 @@
+export { getUserRoles } from './model/selectors/roleSelectors';
+
+export { isUserManager, isUserAdmin } from './model/selectors/roleSelectors';
+
 export { getUserInited } from './model/selectors/getUserInited/getUserInited';
 
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
-export { UserSchema, User } from './model/types/user';
+export { UserSchema, User, UserRole } from './model/types/user';
 export { userActions, userReducer } from './model/slice/userSlice';
