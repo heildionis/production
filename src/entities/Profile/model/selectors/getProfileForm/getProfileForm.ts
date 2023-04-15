532358150f5789906e2179af6a6e28ac89c3ad95
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-15 21:34:04 +0300

    21:34 15.04.2023 Refactored

--- src/entities/Profile/model/selectors/getProfileForm/getProfileForm.ts
+++
@@ -1,3 +0,0 @@
-import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
-
-export const getProfileForm = (state: StateSchema) => state?.profile?.form;
--- src/entities/Profile/model/selectors/getProfileReadonly/getProfileReadonly.ts
+++
@@ -1,3 +0,0 @@
-import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
-
-export const getProfileReadonly = (state: StateSchema) => state?.profile?.readonly;
