3ccbbdcb5964ab6f9aac1bf4de4932f06e528391
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-24 06:12:37 +0300

    06:12 24.04.2023 Extended main props

--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -2,16 +2,24 @@ import { FC, memo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './Icon.module.scss';
 
-interface IconProps {
+interface IconProps extends React.SVGProps<SVGSVGElement> {
    className?: string;
    Svg: React.FC<React.SVGProps<SVGSVGElement>>;
    inverted?: boolean;
 }
 
-export const Icon: FC<IconProps> = memo((props: IconProps) => {
-    const { className, Svg, inverted } = props;
+export const Icon = memo((props: IconProps) => {
+    const {
+        className,
+        Svg,
+        inverted,
+        ...otherProps
+    } = props;
 
     return (
-        <Svg className={classNames(inverted ? cls.inverted : cls.Icon, {}, [className])} />
+        <Svg
+            className={classNames(inverted ? cls.inverted : cls.Icon, {}, [className])}
+            {...otherProps}
+        />
     );
 });
