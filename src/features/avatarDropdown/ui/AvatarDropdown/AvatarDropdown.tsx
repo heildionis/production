2c09ee8e0afe141a21e9a7fc77bbbe24994657e4
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-24 03:41:43 +0300

    03:41 24.04.2023 Created avatarDropdown feature

---
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
@@ -0,0 +1,46 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { memo, useCallback } from 'react';
+import { Avatar } from 'shared/ui';
+import { useSelector } from 'react-redux';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { getUserAuthData, isUserAdmin, isUserManager, userActions } from 'entities/User';
+import { Dropdown } from 'shared/ui/Popups';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import cls from './AvatarDropdown.module.scss';
+
+interface AvatarDropdownProps {
+    className?: string;
+}
+
+export const AvatarDropdown = memo((props: AvatarDropdownProps) => {
+    const { className } = props;
+    const { t } = useTranslation();
+    const authData = useSelector(getUserAuthData);
+    const dispatch = useAppDispatch();
+    const isAdmin = useSelector(isUserAdmin);
+    const isManager = useSelector(isUserManager);
+
+    const isAdminPanelAvailable = isAdmin || isManager;
+
+    const onLogout = useCallback(() => {
+        dispatch(userActions.logout());
+    }, [dispatch]);
+
+    if (!authData) {
+        return null;
+    }
+
+    return (
+        <Dropdown
+            direction='bottom left'
+            className={classNames(cls.AvatarDropdown, {}, [className])}
+            items={[
+                ...(isAdminPanelAvailable ? [{ content: t('Админка'), href: RoutePath.admin_panel }] : []),
+                { content: t('Профиль'), href: RoutePath.profile + authData.id },
+                { content: t('Выйти'), onClick: onLogout },
+            ]}
+            trigger={<Avatar size={30} src={authData.avatar} />}
+        />
+    );
+});
