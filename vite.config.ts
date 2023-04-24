89376977d79305ec4087d969f84b169e516a2fd3
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-24 05:00:50 +0300

    05:00 24.04.2023 Configured vite

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+    plugins: [svgr({ exportAsDefault: true }), react(), tsconfigPaths()],
+    resolve: {
+        alias: [
+            { find: '@', replacement: '/src' },
+        ],
+    },
+    define: {
+        __IS_DEV__: JSON.stringify(true),
+        __API__: JSON.stringify('http://localhost:5000'),
+        __PROJECT__: JSON.stringify('production'),
+    },
+});
