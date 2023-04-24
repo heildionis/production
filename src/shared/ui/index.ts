c175cb5717d551e050a8128a825c37505f4db9f7
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-24 06:11:22 +0300

    06:11 24.04.2023 Created star rating component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -13,3 +13,4 @@ export { Code } from './Code/Code';
 export { Card } from './Card/Card';
 export { Drawer } from './Drawer/Drawer';
 export { Overlay } from './Overlay/Overlay';
+export { StarRating } from './StarRating/StarRating';
