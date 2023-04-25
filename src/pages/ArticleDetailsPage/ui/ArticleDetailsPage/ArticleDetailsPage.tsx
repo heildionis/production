ad58744967f2746e4b7010199811ab715de0a5e0
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-26 01:56:54 +0300

    01:56 26.04.2023 Added article rating feature

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,36 +1,20 @@
-import { ArticleDetails, getArticleDetailsData } from '@/entities/Article';
-import { CommentList } from '@/entities/Comment';
-import { FC, memo, useCallback } from 'react';
+import { FC, memo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { ArticleDetails } from '@/entities/Article';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import {
     DynamicModuleLoader,
     ReducersList,
 } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { Text } from '@/shared/ui';
-import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { useInitialEffect } from '@/shared/lib/hooks/useInitialEffect/useInitialEffect';
-import { AddCommentForm } from '@/features/addCommentForm';
 import { Page } from '@/widgets/Page/Page';
-import { TextSize } from '@/shared/ui/Text/Text';
 import { VStack } from '@/shared/ui/Stack';
 import { ArticleRecommendationsList } from '@/features/articleRecommendationsList';
 import { articleDetailsPageReducer } from '../../model/slices';
-import {
-    fetchCommentsByArticleId,
-} from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
-import {
-    getArticleCommentsIsLoading,
-} from '../../model/selectors/comments/getArticleCommentsIsLoading/getArticleCommentsIsLoading';
-import {
-    getArticleComments,
-} from '../../model/slices/articleDetailsCommentSlice';
-import cls from './ArticleDetailsPage.module.scss';
-import { addCommentForArticle } from '../../model/services/addCommentForArticle/addCommentForArticle';
 import { ArticleDeatilsPageHeader } from '../ArticleDeatilsPageHeader/ArticleDeatilsPageHeader';
 import { ArticleDetailsComments } from '../ArticleDetailsComments/ArticleDetailsComments';
+import cls from './ArticleDetailsPage.module.scss';
+import { ArticleRating } from '@/features/articleRating';
 
 interface ArticleDetailsPageProps {
    className?: string;
@@ -59,6 +43,7 @@ const ArticleDetailsPage: FC<ArticleDetailsPageProps> = memo((props: ArticleDeta
                 <VStack gap='16' fullWidth>
                     <ArticleDeatilsPageHeader />
                     <ArticleDetails id={id} />
+                    <ArticleRating articleId={id} />
                     <ArticleRecommendationsList />
                     <ArticleDetailsComments id={id} />
                 </VStack>
