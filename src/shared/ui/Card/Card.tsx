565b606f168e59c9c2a3f6f966e6da2296cd5b96
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-26 01:55:42 +0300

    01:55 26.04.2023 Refactored

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -4,13 +4,14 @@ import cls from './Card.module.scss';
 
 export enum CardTheme {
     NORMAL = 'normal',
-    OUTLINED = 'outlined'
+    OUTLINED = 'outlined',
 }
 
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
     className?: string;
     children: ReactNode;
     theme?: CardTheme;
+    fullWidth?: boolean;
 }
 
 export const Card: FC<CardProps> = (props: CardProps) => {
@@ -18,14 +19,12 @@ export const Card: FC<CardProps> = (props: CardProps) => {
         className,
         children,
         theme = CardTheme.NORMAL,
+        fullWidth = false,
         ...otherProps
     } = props;
 
     return (
-        <div
-            className={classNames(cls.Card, {}, [className, cls[theme]])}
-            {...otherProps}
-        >
+        <div className={classNames(cls.Card, { [cls.fullWidth]: fullWidth }, [className, cls[theme]])} {...otherProps}>
             {children}
         </div>
     );
