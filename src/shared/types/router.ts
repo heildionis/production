ba0b64dc8fb9edb1bc230563dce01be45aefeb8c
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-04-28 20:57:23 +0300

    20:57 28.04.2023 Added router types

---
+++ src/shared/types/router.ts
@@ -0,0 +1,8 @@
+import { type RouteProps } from 'react-router-dom';
+// eslint-disable-next-line heildionis-plugin/layer-imports
+import { UserRole } from '@/entities/User';
+
+export type AppRouteProps = RouteProps & {
+    authOnly?: boolean;
+    roles?: UserRole[];
+}
