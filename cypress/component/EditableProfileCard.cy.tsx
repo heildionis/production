7044f5523d67277d2964a2359bab01f0c3b9c836
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-05-07 10:04:59 +0300

    10:04 07.05.2023 Added component tests in cypress

---
+++ cypress/component/EditableProfileCard.cy.tsx
@@ -0,0 +1,26 @@
+import { EditableProfileCard } from '@/features/editableProfileCard';
+import { TestProvider } from '@/shared/lib/tests/componentRender/componentRender';
+
+const USER_ID = '2';
+
+describe('EditableProfileCard.cy.tsx', () => {
+    it('playground', () => {
+        cy.intercept('GET', '**/profile/*', { fixture: 'profile.json' });
+        cy.mount(
+            <TestProvider
+                options={{
+                    initialState: {
+                        user: {
+                            authData: {
+                                id: USER_ID,
+                                username: '',
+                            },
+                        },
+                    },
+                }}
+            >
+                <EditableProfileCard id={USER_ID} />
+            </TestProvider>,
+        );
+    });
+});
