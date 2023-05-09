08bede00cc7af15ef120922747aabc15d1b832ee
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-05-09 21:06:11 +0300

    21:06 09.05.2023 Added feature flag helper

---
+++ src/shared/lib/features/toggleFeatures.ts
@@ -0,0 +1,21 @@
+import { getFeatureFlags } from './setGetFeatures';
+
+import { FeatureFlags } from '@/shared/types/featureFlags';
+
+interface ToggleFeaturesOptions<T> {
+    name: keyof FeatureFlags;
+    on: () => T;
+    off: () => T;
+}
+
+export function toggleFeatures<T>({
+    name,
+    on,
+    off,
+}: ToggleFeaturesOptions<T>): T {
+    if (getFeatureFlags(name)) {
+        return on();
+    }
+
+    return off();
+}
