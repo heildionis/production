2a3d0faa1f4412a2d3643146eeda71ceaec8d9d1
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-05-14 22:24:25 +0300

    22:24 14.05.2023 Started project redesign

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,54 @@
 import { Suspense, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { AppRouter } from './providers/router';
 
-import { userActions, getUserInited } from '@/entities/User';
+import { getUserInited, initAuthData } from '@/entities/User';
+import { MainLayout } from '@/shared/layouts/MainLayout';
 import { classNames } from '@/shared/lib/classNames/classNames';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { Navbar } from '@/widgets/Navbar';
+import { PageLoader } from '@/widgets/PageLoader';
 import { Sidebar } from '@/widgets/Sidebar';
 
 export const App = () => {
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
     const inited = useSelector(getUserInited);
 
     useEffect(() => {
-        dispatch(userActions.initAuthData());
+        dispatch(initAuthData());
     }, [dispatch]);
 
+    if (!inited) {
+        return <PageLoader />;
+    }
+
     return (
-        <div className={classNames('app', {})}>
-            <Suspense fallback=''>
-                <Navbar />
-                <div className='content-page'>
-                    <Sidebar />
-                    {inited && <AppRouter />}
+        <ToggleFeatures
+            feature='isAppRedesigned'
+            off={
+                <div className={classNames('app', {})}>
+                    <Suspense fallback=''>
+                        <Navbar />
+                        <div className='content-page'>
+                            <Sidebar />
+                            {inited && <AppRouter />}
+                        </div>
+                    </Suspense>
                 </div>
-            </Suspense>
-        </div>
+            }
+            on={
+                <div className={classNames('app_redesigned', {})}>
+                    <Suspense fallback=''>
+                        <MainLayout
+                            header={<Navbar />}
+                            content={<AppRouter />}
+                            sidebar={<Sidebar />}
+                        />
+                    </Suspense>
+                </div>
+            }
+        />
     );
 };
